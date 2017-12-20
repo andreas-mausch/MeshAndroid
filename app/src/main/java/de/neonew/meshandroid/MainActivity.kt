c1753ca3ab3e87cf7a59dab374e012695135bc7a
neonew <neonew@gmail.com>
2017-12-20 23:12:54 +0100

    basic layout for gui

--- app/src/main/java/de/neonew/meshandroid/MainActivity.kt
+++ app/src/main/java/de/neonew/meshandroid/MainActivity.kt
@@ -3,9 +3,12 @@ package de.neonew.meshandroid
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.makeText
 import kotlinx.android.synthetic.main.activity_main.*
 import org.apache.commons.io.IOUtils
 import java.io.IOException
+
 
 class MainActivity : Activity() {
 
@@ -13,32 +16,35 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ip.setText("10.0.0.1")
+
+        start_mesh.setOnClickListener {
+            try {
+                startMesh(ip.text.toString())
+            } catch (e: IOException) {
+                makeText(this, e.message, LENGTH_LONG).show()
+            }
+
+            Log.i("MainActivity", "OK")
+        }
+    }
+
+    private fun startMesh(ip: String) {
         runAsRoot("ifconfig wlan0 down")
         runAsRoot("echo \"/system/etc/wifi/bcmdhd_ibss.bin\" >> /sys/module/dhd/parameters/firmware_path")
         runAsRoot("echo \"/system/etc/wifi/nvram_net.txt\" >> /sys/module/dhd/parameters/nvram_path")
-        runAsRoot("ifconfig wlan0 10.0.0.1 netmask 255.255.0.0 up")
+        runAsRoot("ifconfig wlan0 ${ip} netmask 255.255.0.0 up")
         runAsRoot("iw wlan0 set type ibss")
         runAsRoot("iw wlan0 ibss join MeshAndroid 2412")
-
-        Log.i("MainActivity", "OK")
-
-        sample_text.text = stringFromJNI()
     }
 
     private fun runAsRoot(command: String) {
-        try {
-            val process = Runtime.getRuntime().exec(arrayOf("su", "-c", command))
-            val returnValue = process.waitFor()
-            val output = IOUtils.toString(process.inputStream)
-            if (returnValue != 0) {
-                throw IllegalStateException("command failed: $command; ret: $returnValue; output: $output")
-            }
-        } catch (e: IOException) {
-            throw RuntimeException(e)
-        } catch (e: InterruptedException) {
-            throw RuntimeException(e)
+        val process = Runtime.getRuntime().exec(arrayOf("su", "-c", command))
+        val returnValue = process.waitFor()
+        val output = IOUtils.toString(process.inputStream)
+        if (returnValue != 0) {
+            throw IllegalStateException("command failed: $command; ret: $returnValue; output: $output")
         }
-
     }
 
     /**
