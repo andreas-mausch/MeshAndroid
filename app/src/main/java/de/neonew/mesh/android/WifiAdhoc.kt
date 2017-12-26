94fbbbac331b3ee529fd8f015fdaab51ca20fbdd
neonew <neonew@gmail.com>
2017-12-26 04:17:46 +0100

    static imports

--- app/src/main/java/de/neonew/mesh/android/WifiAdhoc.kt
+++ app/src/main/java/de/neonew/mesh/android/WifiAdhoc.kt
@@ -1,14 +1,16 @@
 package de.neonew.mesh.android
 
+import de.neonew.mesh.android.Runner.Companion.runAsRoot
+
 class WifiAdhoc {
     companion object {
         fun startMesh(ip: String, name: String) {
-            Runner.runAsRoot("ifconfig wlan0 down")
-            Runner.runAsRoot("echo \"/system/etc/wifi/bcmdhd_ibss.bin\" >> /sys/module/dhd/parameters/firmware_path")
-            Runner.runAsRoot("echo \"/system/etc/wifi/nvram_net.txt\" >> /sys/module/dhd/parameters/nvram_path")
-            Runner.runAsRoot("ifconfig wlan0 ${ip} netmask 255.255.0.0 up")
-            Runner.runAsRoot("iw wlan0 set type ibss")
-            Runner.runAsRoot("iw wlan0 ibss join ${name} 2412 00:11:22:33:44:55")
+            runAsRoot("ifconfig wlan0 down")
+            runAsRoot("echo \"/system/etc/wifi/bcmdhd_ibss.bin\" >> /sys/module/dhd/parameters/firmware_path")
+            runAsRoot("echo \"/system/etc/wifi/nvram_net.txt\" >> /sys/module/dhd/parameters/nvram_path")
+            runAsRoot("ifconfig wlan0 ${ip} netmask 255.255.0.0 up")
+            runAsRoot("iw wlan0 set type ibss")
+            runAsRoot("iw wlan0 ibss join ${name} 2412 00:11:22:33:44:55")
         }
     }
 }
