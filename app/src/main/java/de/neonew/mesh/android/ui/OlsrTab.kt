a1424736244e3580b8c59a9d14bf609398fd7781
neonew <neonew@gmail.com>
2018-01-08 05:22:02 +0100

    use onViewCreated() for Fragment initialization

--- app/src/main/java/de/neonew/mesh/android/ui/OlsrTab.kt
+++ app/src/main/java/de/neonew/mesh/android/ui/OlsrTab.kt
@@ -10,7 +10,6 @@ import android.widget.ArrayAdapter
 import de.neonew.mesh.android.Olsrd
 import de.neonew.mesh.android.R
 import kotlinx.android.synthetic.main.olsr_tab.*
-import kotlinx.android.synthetic.main.olsr_tab.view.*
 
 
 class OlsrTab : Fragment() {
@@ -20,29 +19,32 @@ class OlsrTab : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.olsr_tab, container, false)
+        return inflater.inflate(R.layout.olsr_tab, container, false)
+    }
 
-        view.olsrd_run.setOnClickListener {
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        olsrd_run.setOnClickListener {
             Olsrd().run(context)
             update()
         }
 
-        view.olsrd_kill.setOnClickListener {
+        olsrd_kill.setOnClickListener {
             Olsrd().kill()
             update()
         }
 
-        view.olsrd_force_kill.setOnClickListener {
+        olsrd_force_kill.setOnClickListener {
             Olsrd().forceKill()
             update()
         }
 
         neighborsAdapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, emptyList<String>().toMutableList())
-        view.olsrd_neighbors.adapter = neighborsAdapter
+        olsrd_neighbors.adapter = neighborsAdapter
 
         handler.post(updateRunnable)
 
-        return view
     }
 
     override fun onDestroyView() {
--- app/src/main/java/de/neonew/mesh/android/ui/WifiTab.kt
+++ app/src/main/java/de/neonew/mesh/android/ui/WifiTab.kt
@@ -9,19 +9,22 @@ import android.widget.Toast
 import de.neonew.mesh.android.R
 import de.neonew.mesh.android.WifiAdhoc.Companion.startMesh
 import kotlinx.android.synthetic.main.wifi_tab.*
-import kotlinx.android.synthetic.main.wifi_tab.view.*
 import java.io.IOException
 
 class WifiTab : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.wifi_tab, container, false)
+        return inflater.inflate(R.layout.wifi_tab, container, false)
+    }
 
-        view.ip.setText("10.0.0.1")
-        view.name.setText("MeshAndroid")
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        view.start_mesh.setOnClickListener {
+        ip.setText("10.0.0.1")
+        name.setText("MeshAndroid")
+
+        start_mesh.setOnClickListener {
             try {
                 startMesh(ip.text.toString(), name.text.toString())
             } catch (e: IOException) {
@@ -31,7 +34,5 @@ class WifiTab : Fragment() {
                 wifi_status.update()
             }
         }
-
-        return view
     }
 }
